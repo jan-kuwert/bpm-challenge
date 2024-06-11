55040f1846e351be07e4613c1fc87be5968b2626
Jan Kuwert <jan.kuwert@tum.de>
2024-06-11 15:19:43 +0200

    add enum and basic intake

--- simulator.py
+++ simulator.py
@@ -1,13 +1,25 @@
 #! /usr/bin/python3
 import requests
-import json
+from enum import Enum, auto
 from bottle import  route, run, template, post, get, put, delete
+
+class InstanceType(Enum):
+    fork_running = auto()
+    fork_ready = auto()
+    wait_running = auto()
+    wait_ready = auto()
 
 @get('/names/<name>')
 def index(name):
     return template('<b>Hello {name} </>!', name=name)
 
-def new_instance(behavior='fork_running', type='A'):
+@get('/intake')
+def index():
+
+    return 
+
+
+def new_instance(behavior:InstanceType='fork_running', type='A'):
     url = "https://cpee.org/flow/start/url/"
     xml_url = "https://cpee.org/hub/server/Teaching.dir/Prak.dir/Challengers.dir/Jan_Kuwert.dir/hospital_test.xml"
     data = {
