37a4ff7ea168b1a092ac7342da00a9bc17737081
jan kuwert <jan.kuwert@gmx.de>
2024-06-14 16:12:34 +0200

    add logging

--- simulator.py
+++ simulator.py
@@ -20,22 +20,26 @@ RESOURCES = [
 PATIENT_IDS = 0
 PATIENT_QUEUE = []
 CURRENT_TIME = 60 * 8  # 8:00 AM
+
 INSTANCE_TYPES = ["fork_running", "fork_ready", "wait_running", "wait_ready"]
 DIAGNOSIS_TYPES = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"]
 PATIENT_TYPES = ["A", "B", "EM"]
+
 INTAKE_TIME = [1, 0.125]
 SURGERY_TIME = [
-    [0, 0],
+    [False],
     [1, 0.25],
     [2, 0.5],
     [4, 0.5],
-    [0, 0],
-    [0, 0],
+    [False],
+    [False],
     [4, 0.5],
     [4, 1],
 ]
 NURSING_TIME = [[4, 0.5], [8, 2], [16, 2], [16, 2], [8, 2], [16, 2], [16, 4], [16, 4]]
 EMERGENCY_TIME = [2, 0.5]
+COMPLICATION_PROBABILITY = [0.01, 0.01, 0.02, 0.02, 0.001, 0.001, 0.002, 0.002]
+PHANTOM_PAIN_PROBABILITY = 0.01
 
 
 @post("/patient_admission")
@@ -60,13 +64,16 @@ def patient_admission():
             elif patientData["type"] == "EM":
                 patientData["resources"] = "EM"
             patientData["resources_available"] = True  # TODO implement
+            patientData["complications"] = False
+            patientData["phantom_pain"] = False
             patientData["id"] = add_patient(patientData)
         else:
             patientData = get_patient(patientData["id"])
 
-        print("Patient Data:", patientData)
+        set_log(patientData, "patient_admission")
         return patientData
     except Exception as e:
+        set_log(patientData, "patient_admission_error", e)
         print("patient_admission_error: ", e)
         return e
 
@@ -82,9 +89,10 @@ def replan_patient():
         # response = create_instance(patientData) # TODO implement new instance management
         response = True
         set_patient(patientData)
-        print("Patient Replanned:", patientData)
+        set_log(patientData, "replan_patient")
         return response
     except Exception as e:
+        set_log(patientData, "replan_patient_error", e)
         print("replan_patient_error: ", e)
         return e
 
@@ -99,9 +107,10 @@ def intake():
         patientData["total_time"] = str(
             float(patientData["total_time"]) + np.random.normal(mean, sigma)
         )
-        print("Intake Time:", patientData["total_time"])
+        set_log(patientData, "intake")
         return
     except Exception as e:
+        set_log(patientData, "intake_error", e)
         print("intake_error: ", e)
         return e
 
@@ -113,9 +122,11 @@ def er_treatmentr():
         mean = request.forms.get("mean", EMERGENCY_TIME[0])
         sigma = request.forms.get("sigma", EMERGENCY_TIME[1])
         patientData["total_time"] += np.random.normal(mean, sigma)
-        print("ER Time:", patientData["total_time"])
-        return
+        patientData["phantom_pain"] = evaluate_probability(PHANTOM_PAIN_PROBABILITY)
+        set_log(patientData, "er_treatment")
+        return patientData
     except Exception as e:
+        set_log(patientData, "er_treatment_error", e)
         print("er_treatment_error: ", e)
         return e
 
@@ -127,9 +138,10 @@ def surgery():
         mean = SURGERY_TIME[get_patient_type_index(patientData["type"])][0]
         sigma = SURGERY_TIME[get_patient_type_index(patientData["type"])][1]
         patientData["total_time"] += np.random.normal(mean, sigma)
-        print("Surgery Time:", patientData["total_time"])
+        set_log(patientData, "surgery")
         return
     except Exception as e:
+        set_log(patientData, "surgery_error", e)
         print("surgery_error: ", e)
         return e
 
@@ -141,9 +153,13 @@ def nursing():
         mean = NURSING_TIME[get_patient_type_index(patientData["type"])][0]
         sigma = NURSING_TIME[get_patient_type_index(patientData["type"])][1]
         patientData["total_time"] += np.random.normal(mean, sigma)
-        print("Nursing Time:", patientData["total_time"])
+        patientData["complications"] = evaluate_probability(
+            COMPLICATION_PROBABILITY[get_patient_type_index(patientData["type"])]
+        )
+        set_log(patientData, "nursing")
         return
     except Exception as e:
+        set_log(patientData, "nursing_error", e)
         print("nursing_error: ", e)
         return e
 
@@ -152,9 +168,11 @@ def nursing():
 def releasing():
     try:
         patientData = get_patient(request.forms.get("id"))
-        print("Patient Released:", patientData)
+        set_log(patientData, "releasing")
+        write_log_to_txt(patientData["id"])
         return
     except Exception as e:
+        set_log(patientData, "releasing_error", e)
         print("releasing_error: ", e)
         return e
 
@@ -181,7 +199,9 @@ def create_database():
             diagnosis TEXT,
             scheduled TEXT,
             resources TEXT,
-            resources_available TEXT
+            resources_available TEXT,
+            complications TEXT,
+            phantom_pain TEXT
         )
         """
     )
@@ -192,6 +212,17 @@ def create_database():
             name TEXT NOT NULL,
             current INTEGER NOT NULL,
             max INTEGER NOT NULL
+        )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS logs(
+            id INTEGER PRIMARY KEY,
+            patientId INTEGER NOT NULL,
+            patientData TEXT NOT NULL,
+            tasks INTEGER NOT NULL,
+            error TEXT
         )
         """
     )
@@ -207,7 +238,7 @@ def add_patient(patientData):
         cursor = connection.cursor()
         cursor.execute(
             """
-            INSERT INTO patients(type, admission_time, start_time, total_time, diagnosis, scheduled, resources, resources_available)
+            INSERT INTO patients(type, admission_time, start_time, total_time, diagnosis, scheduled, resources, resources_available, complications, phantom_pain)
             VALUES(?, ?, ?, ?, ?, ?, ?, ?)
             """,
             (
@@ -219,6 +250,8 @@ def add_patient(patientData):
                 patientData["scheduled"],
                 patientData["resources"],
                 patientData["resources_available"],
+                patientData["complications"],
+                patientData["phantom_pain"],
             ),
         )
         connection.commit()
@@ -253,6 +286,8 @@ def get_patient(patientId):
         patientData["scheduled"] = patient[6]
         patientData["resources"] = patient[7]
         patientData["resources_available"] = patient[8]
+        patientData["complications"] = patient[9]
+        patientData["phantom_pain"] = patient[10]
         return patientData
     except Exception as e:
         print("get_patient_error: ", e)
@@ -267,7 +302,7 @@ def set_patient(patientData):
         cursor.execute(
             """
             UPDATE patients
-            SET type = ?, admission_time = ?, start_time =?, total_time =?, diagnosis = ?, scheduled = ?, resources = ?, resources_available = ?
+            SET type = ?, admission_time = ?, start_time =?, total_time =?, diagnosis = ?, scheduled = ?, resources = ?, resources_available = ?, complications = ?, phantom_pain = ?
             WHERE id = ?
             """,
             (
@@ -279,6 +314,8 @@ def set_patient(patientData):
                 patientData["scheduled"],
                 patientData["resources"],
                 patientData["resources_available"],
+                patientData["complications"],
+                patientData["phantom_pain"],
                 patientData["id"],
             ),
         )
@@ -314,8 +351,115 @@ def add_resource(resource):
     return
 
 
+# returns resource from the database
+def get_resource(resource_name):
+    try:
+        connection = sqlite3.connect("hospital.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            SELECT * FROM resources
+            WHERE name = ?
+            """,
+            (resource_name,),
+        )
+        resource = cursor.fetchone()
+        connection.close()
+        resourceData = {}
+        resourceData["id"] = resource[0]
+        resourceData["name"] = resource[1]
+        resourceData["current"] = resource[2]
+        resourceData["max"] = resource[3]
+        return resourceData
+    except Exception as e:
+        print("get_resource_error: ", e)
+        return
+
+
+# update resource in the db
+def set_resource(resourceData):
+    try:
+        connection = sqlite3.connect("hospital.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            UPDATE resources
+            SET name = ?, current = ?, max = ?
+            WHERE id = ?
+            """,
+            (
+                resourceData["name"],
+                resourceData["current"],
+                resourceData["max"],
+                resourceData["id"],
+            ),
+        )
+        connection.commit()
+        connection.close()
+        return
+    except Exception as e:
+        print("set_resource_error: ", e)
+        return
+
+
+def set_log(patientData, task, error=None):
+    try:
+        connection = sqlite3.connect("hospital.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            INSERT INTO logs(patientId, patientData, tasks, error)
+            VALUES(?, ?, ?)
+            """,
+            (patientData["id"], patientData, task, error),
+        )
+        connection.commit()
+        connection.close()
+        print("Log Set:", patientData["id"], patientData, task, error)
+        return cursor.lastrowid
+    except Exception as e:
+        print("set_log_error: ", e)
+    return
+
+
+def get_log(patientId):
+    try:
+        connection = sqlite3.connect("hospital.db")
+        cursor = connection.cursor()
+        if patientId:
+            cursor.execute(
+                """
+                SELECT * FROM logs
+                WHERE patientId = ?
+                """,
+                (patientId,),
+            )
+        else:
+            cursor.execute(
+                """
+                SELECT * FROM logs
+                """
+            )
+        logs = cursor.fetchall()
+        connection.close()
+        return logs
+    except Exception as e:
+        print("get_log_error: ", e)
+        return
+
+
+def write_log_to_txt(patientId=None):
+    try:
+        log = get_log(patientId)
+        with open(f"log-{patientId}.txt", "a") as file:
+            file.write(log)
+    except Exception as e:
+        print("write_log_to_txt_error: ", e)
+        return
+
+
 # returns True if patient has complications for the given probability
-def check_for_complications(probability):
+def evaluate_probability(probability):
     return pr.prob(probability)
 
 
