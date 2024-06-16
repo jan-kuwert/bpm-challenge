5a60d8adb1e901730e174174da1f25a0aa7962c3
jan kuwert <jan.kuwert@gmx.de>
2024-06-16 18:31:03 +0200

    .

--- simulator.py
+++ simulator.py
@@ -82,7 +82,7 @@ def patient_admission():
 
                 set_resource(resource)
                 set_patient(patient_data)
-
+            print("patient_admission: ", resource, patient_data[resource])
         set_log(patient_data, "patient_admission")
         return patient_data
     except Exception as e:
