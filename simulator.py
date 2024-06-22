1a8de4c5831bf15d24e2953acc15e6fcf634f7fe
jan kuwert <jan.kuwert@gmx.de>
2024-06-22 13:26:45 +0200

    .

--- simulator.py
+++ simulator.py
@@ -17,7 +17,7 @@ def handle_task_async():
     try:
         # Get the callback url from the request
         callback_url = request.headers["CPEE-CALLBACK"]
-        data = request.forms.get('id')
+        data = request.query.get('id')
         print("data: ", data)
         # start the task execution
         executor.submit(task, data, callback_url)
