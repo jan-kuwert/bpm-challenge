d884b31125ba19e3efe7716b49fd52cfecd32f1e
Jan Kuwert <jan.kuwert@tum.de>
2024-06-24 12:07:26 +0200

    .

--- simulator.py
+++ simulator.py
@@ -389,8 +389,6 @@ def create_instance(entity, behavior="fork_running"):
             "url": xml_url,
             "init": '{"id": "'
             + str(entity["id"])
-            + ","
-            + str(entity).replace("{", "").replace("}", "")
             + '"}',
         }
 
