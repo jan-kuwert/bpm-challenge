35d02fcf5ece0218ab05ded5ceda260b6451d1c0
jan kuwert <jan.kuwert@gmx.de>
2024-08-05 00:09:32 +0200

    update tabusearch

--- provided/tabusearch.py
+++ provided/tabusearch.py
@@ -1,4 +1,5 @@
 import random as rd
+from itertools import combinations
 
 
 class TabuSearch:
@@ -11,6 +12,12 @@ class TabuSearch:
         self.tabu_list = []
         self.initial_solution = self.get_initial_solution()
         self.instance_dict = plannable_elements
+
+    def get_tabu_list(self):
+        dict = {}
+        for swap in combinations(self.instance_dict.keys(), 2):
+            dict[swap] = {"tabu_time": 0, "MoveValue": 0}
+        return dict
 
     def get_initial_solution(self):
         """
@@ -29,28 +36,27 @@ class TabuSearch:
         make cost less the shorter patient has to wait (expontial: very low for 1/2 days a lot more for 5/6 etc)
         make cost of already replanned patient higher and for multiple replans extreme
         multiply with factor for ER patients
-        
         """
+
         cost = 0
         for i in range(len(current_state)):
             for j in range(i + 1, len(current_state)):
                 if current_state[i] > current_state[j]:
                     cost += 1
         return cost
-    
 
-    def get_neighbourhood(self, current_state):
+    def SwapMove(self, solution, i, j):
         """
-        :param current_state: dict of current state of the problem from simulator
-        :return: list of all possible moves from current state
+        Takes a list (solution)
+        returns a new neighbor solution with i, j swapped
         """
-        neighbourhood = []
-        for i in range(len(current_state)):
-            for j in range(i + 1, len(current_state)):
-                neighbour = current_state.copy()
-                neighbour[i], neighbour[j] = neighbour[j], neighbour[i]
-                neighbourhood.append(neighbour)
-        return neighbourhood
+        solution = solution.copy()
+
+        i_index = solution.index(i)
+        j_index = solution.index(j)
+
+        solution[i_index], solution[j_index] = solution[j_index], solution[i_index]
+        return solution
 
     def Tsearch(self):
         """
@@ -58,4 +64,79 @@ class TabuSearch:
         """
         tenure = self.tabu_tenure
         best_solution = self.initial_solution
+        current_solution = self.initial_solution
+        best_cost = self.get_cost(best_solution)
+        tabu_list = self.get_tabu_list()
 
+        iter = 1
+        Terminate = 0
+
+        while Terminate < 5:
+
+            for move in tabu_list:
+                candidate_solution = self.SwapMove(current_solution, move[0], move[1])
+                candidate_cost = self.get_cost(candidate_solution)
+                tabu_list[move]["MoveValue"] = candidate_cost
+
+            while True:
+                best_move = min(tabu_list, key=lambda x: tabu_list[x]["MoveValue"])
+                MoveValue = tabu_list[best_move]["MoveValue"]
+                tabu_time = tabu_list[best_move]["tabu_time"]
+
+                if tabu_time < iter:
+                    current_solution = self.SwapMove(
+                        current_solution, best_move[0], best_move[1]
+                    )
+                    current_cost = self.Objfun(current_solution)
+
+                    if MoveValue < best_cost:
+                        best_solution = current_solution
+                        best_cost = current_cost
+                        print(
+                            "   best_move: {}, Cost: {} => Best Improving => Admissible".format(
+                                best_move, current_cost
+                            )
+                        )
+                        Terminate = 0
+                    else:
+                        print(
+                            "   ##Termination: {}## best_move: {}, Cost: {} => Least non-improving => "
+                            "Admissible".format(Terminate, best_move, current_cost)
+                        )
+                        Terminate += 1
+
+                    tabu_list[best_move]["tabu_time"] = iter + tenure
+                    iter += 1
+                    break
+                else:
+                    if MoveValue < best_cost:
+
+                        current_solution = self.SwapMove(
+                            current_solution, best_move[0], best_move[1]
+                        )
+                        current_cost = self.Objfun(current_solution)
+                        best_solution = current_solution
+                        best_cost = current_cost
+                        print(
+                            "   best_move: {}, Cost: {} => Aspiration => Admissible".format(
+                                best_move, current_cost
+                            )
+                        )
+                        Terminate = 0
+                        iter += 1
+                        break
+                    else:
+                        tabu_list[best_move]["MoveValue"] = float("inf")
+                        print(
+                            "   best_move: {}, Cost: {} => Tabu => Inadmissible".format(
+                                best_move, current_cost
+                            )
+                        )
+                        continue
+        print(
+            "#" * 50,
+            "Performed iterations: {}".format(iter),
+            "Best found Solution: {} , Cost: {}".format(best_solution, best_cost),
+            sep="\n",
+        )
+        return tabu_list, best_solution, best_cost
